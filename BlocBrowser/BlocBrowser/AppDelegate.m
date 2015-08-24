7c6202bdc4343ea9705cfc707726d8b85f5d6d4c
Weinan Qiu <davidiamyou@gmail.com>
2015-08-24 15:40:30 -0400

    Checkpoint 24 Assignment

--- BlocBrowser/BlocBrowser/AppDelegate.m
+++ BlocBrowser/BlocBrowser/AppDelegate.m
@@ -15,6 +15,7 @@
 
 @implementation AppDelegate
 
+#pragma mark AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
@@ -35,9 +36,7 @@
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     
-    UINavigationController *nvc = (UINavigationController *) self.window.rootViewController;
-    ViewController *viewController = [[nvc viewControllers] firstObject];
-    [viewController resetWebView];
+    [[self viewController] resetWebView];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -51,10 +50,29 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    [self presentWelcomeMessage];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma mark Misc
+
+- (ViewController *) viewController {
+    UINavigationController *nvc = (UINavigationController *) self.window.rootViewController;
+    return [[nvc viewControllers] firstObject];
+}
+
+- (void) presentWelcomeMessage {
+    UIAlertController *welcomeAlert = [UIAlertController alertControllerWithTitle:@"Welcome"
+                                                                          message:NSLocalizedString(@"Welcome to BlocBrowser", nil)
+                                                                   preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil];
+    [welcomeAlert addAction:okAction];
+    
+    [[self viewController] presentViewController:welcomeAlert animated:YES completion:nil];
 }
 
 @end
